bf10bb23e16157253f5d11ec2c371fd15e10d737
Sahar Qasemi <sahar.gh9244@gmail.com>
2023-06-03 06:45:49 +0430

    create simple weather react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Weather from "./Weather";
+
+let weatherData = {
+  city: "New York",
+  temperature: 19,
+  description: "Cloudy",
+  imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+  humidity: 80,
+  wind: 10,
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Weather
+        city={weatherData.city}
+        temp={weatherData.temperature}
+        desc={weatherData.description}
+        icon={weatherData.imgUrl}
+        humidity={weatherData.humidity}
+        wind={weatherData.wind}
+      />
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+
+import "./Weather.css";
+
+import axios from "axios";
+
+function Weather(props) {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      city: response.data.name,
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "58a6775f97527351bf6c6966e209be39";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  let form = (
+    <form onSubmit={handleSubmit} className="form">
+      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
+      <button type="Submit">Search</button>
+    </form>
+  );
+  if (loaded) {
+    return (
+      <div>
+        <div className="container">
+          {form}
+          <div className="row">
+            <div className="col-md-6">
+              <p>{weather.city}</p>
+              <p>Temperature: {Math.round(weather.temperature)}°C</p>
+              <p>Description: {weather.description}</p>
+              <p>
+                <img src={weather.icon} alt={weather.description} />
+              </p>
+            </div>
+            <div className="col-md-6 control">
+              <p>Humidity: {weather.humidity}%</p>
+              <p>Wind: {weather.wind}km/h</p>
+            </div>
+          </div>
+        </div>
+        <small>
+          <a
+            href="https://github.com/SaharQ1986/Vanilla-Weather-Search-Project.git"
+            target="_blank"
+          >
+            Open-source code
+          </a>
+          by Sahar.Q
+        </small>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        {form}
+        <div className="row">
+          <div className="col-md-6">
+            <p>{props.city}</p>
+            <p>Temperature: {Math.round(props.temp)}°C</p>
+            <p>Description: {props.desc}</p>
+            <p>
+              <img src={props.icon} alt={props.description} />
+            </p>
+          </div>
+          <div className="col-md-6 control">
+            <p>Humidity: {props.humidity}%</p>
+            <p>Wind: {props.wind}km/h</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Weather;
